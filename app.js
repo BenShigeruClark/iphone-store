0fb078fad4278b91cecb8a7da83660972e00bf0e
Ben Clark <benclarkphoto@mac.com>
2022-07-02 22:54:16 -0700

    add scrolltrigger to page 2 fade in text as you scroll, then fade out to 0% once text is almost at 20%

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
-const tlIntro = gsap.timeline({
+// const tlIntro = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".first-page",
+//         start: "05%",
+//         end: "25%",
+//         scrub: true,
+//     },
+// });
+
+// tlIntro.fromTo('nav', {opacity: 1}, {opacity: 0})
+
+// Highlight Page 2
+const tlH = gsap.timeline({
     scrollTrigger: {
-        trigger: ".first-page",
-        start: "050%",
-        end: "25%",
+        trigger: ".second-page",
+        markers: {startColor: "blue", endColor: "green"},
         scrub: true,
-    },
+        start: "-40%",
+        end: "40%",
+    }
 });
 
-tlIntro.fromTo('nav', {opacity: 1}, {opacity: 0})
+tlH.fromTo(
+    ".highlight", 
+    {color: "rgba(255,255,255,0.4)"},
+    {color: "rgba(255,255,255,1", stagger: 1}
+    );
+
+const tlHRemove = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".second-page",
+        markers: {startColor: "pink", endColor: "red"},
+        scrub: true,
+        start: "-20%",
+        end: "60%",
+    }
+});
+
+tlHRemove.fromTo(
+    ".highlight",
+    {color: "rgba(255,255,255,1"},
+    {color: "rgba(255,255,255,0", stagger: 1}
+);
