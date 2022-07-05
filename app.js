f69fce5c496f2e81a4c7086feb1de7aff3f6e80a
Ben Clark <benclarkphoto@mac.com>
2022-07-04 21:58:40 -0700

    add scroll over from third to fourth page

--- app.js
+++ app.js
@@ -69,3 +69,13 @@ tlSplit.fromTo(".large-phone", { x: "40%"}, { x: "20%"});
 tlSplit.fromTo(".small-phone", {x: "-40%"}, { x: "-20%"}, "<");
 tlSplit.fromTo(".product-text-left", { x: 50, opacity: 0 }, { opacity: 1, x: 0 }, "<");
 tlSplit.fromTo(".product-text-right", { x: -50, opacity: 0 }, { opacity: 1, x: 0 }, "<");
+
+const tlSplitPin = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".third-page",
+        pin: true,
+        pinSpacing: false,
+        start: "0%",
+        end: "100%",
+    }
+});
